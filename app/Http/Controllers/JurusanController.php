7238f633852839575a7ce7ba8e5fb2eb800f26c9
Mulkian <145630818+Mulkian@users.noreply.github.com>
2024-05-06 11:39:23 +0700

    edit jurusan

--- app/Http/Controllers/JurusanController.php
+++ app/Http/Controllers/JurusanController.php
@@ -12,8 +12,9 @@ class JurusanController extends Controller
      */
     public function index()
     {
+        $nomor = 1;
         $jur = Jurusan::all();
-        return view('jurusan.index',compact('jur'));
+        return view('jurusan.index',compact('nomor','jur'));
     }
 
     /**
@@ -29,7 +30,12 @@ class JurusanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $jur = new Jurusan;
+        $jur->kode = $request->kode;
+        $jur->jurusan = $request->nama;
+        $jur->save();
+
+        return redirect('/jurusan/');
     }
 
     /**
@@ -46,7 +52,8 @@ class JurusanController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $jur = Jurusan::find($id);
+        return view('jurusan.edit',compact('jur'));
     }
 
     /**
@@ -54,7 +61,12 @@ class JurusanController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $jur = Jurusan::find($id);
+        $jur->kode = $request->kode;
+        $jur->jurusan = $request->nama;
+        $jur->save();
+
+        return redirect('/jurusan/');
     }
 
     /**
--- resources/views/jurusan/edit.blade.php
+++ resources/views/jurusan/edit.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
-@section('title','Tambah Jurusan')
-@section('judul','Tambah Jurusan')
+@section('title','Edit Jurusan')
+@section('judul','Edit Jurusan')
 @section('bc')
     <ol class="breadcrumb float-sm-right">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
         <li class="breadcrumb-item"><a href="#">Data Jurusan</a></li>
-        <li class="breadcrumb-item active">Tambah Jurusan</li>
+        <li class="breadcrumb-item active">Edit Jurusan</li>
     </ol>
 @endsection
 
@@ -24,17 +24,18 @@
         </div>
         </div>
         <div class="card-body">
-            <form method="post" action="/jurusan/store/">
+            <form method="post" action="/jurusan/{{$jur->id}}">
+                @method('PUT')
                 @csrf
                 <div class="mb-3">
                     <label class="form-label">Kode Jurusan</label>
-                    <input type="text" class="form-control" name="kode">
+                    <input type="text" readonly value="{{$jur->kode}}" class="form-control" name="kode">
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Jurusan</label>
-                    <input type="text" class="form-control" name="nama">
+                    <input type="text" value="{{$jur->jurusan}}" class="form-control" name="nama">
                 </div>
-                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                <button type="submit" class="btn btn-primary">Edit Data</button>
             </form>
         </div>
         <!-- /.card-body -->
